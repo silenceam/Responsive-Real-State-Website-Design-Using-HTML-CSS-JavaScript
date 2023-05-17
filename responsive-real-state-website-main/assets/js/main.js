fa68e400aa15d774e0bbad7a00c643bc75a0c943
Silence <76023488+silenceam@users.noreply.github.com>
2023-05-17 11:19:59 +0700

    JavaScript

--- responsive-real-state-website-main/assets/js/main.js
+++ responsive-real-state-website-main/assets/js/main.js
@@ -13,7 +13,7 @@ var swiperPopular = new Swiper(".popular__container", {
   spacebetween: 32,
   grabCrusor: true,
   centeredSlides: true,
-  slidesPreView: 'auto',
+  slidesPerView: 'auto',
   loop: true,
 
   navigation: {
@@ -26,16 +26,16 @@ var swiperPopular = new Swiper(".popular__container", {
 const accordionItems = document.querySelectorAll('.value__accordion-item')
 
 accordionItems.forEach((item) => {
-  const accordionHeader = item.querySelector('.value__accordion-header')
+  const accordionHeader = item.querySelector('.value__accordion-header');
 
   accordionHeader.addEventListener('click' , ( )=> {
 
     const openItem =document.querySelector('.accordion-open')
 
-    toggleItem(item)
+    toggleItem(item);
 
     if (openItem && openItem !== item) {
-      toggleItem(openItem)
+      toggleItem(openItem);
     }
   })
 })
@@ -71,7 +71,58 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // when the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-
+  if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' :'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the calidation is fulfilled, we ask what was the issue to know if we activated or deactivated
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate/deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selectedTheme', getCurrentTheme())
+  localStorage.setItem('selectedIcon', getCurrentIcon())
+})
+
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay:400,
+  reset: true
+})
+
+sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`)
+sr.reveal(`.home__description, .footer__info`,{delay:400})
+sr.reveal(`.home__search`,{delay:500})
+sr.reveal(`.home__value`,{delay:600})
+sr.reveal(`.home__images`,{delay:700, origin: 'bottom'})
+sr.reveal(`.logos__img`, {interval: 100})
+sr.reveal(`.value__images, .contact__content`,{origin: 'left'})
+sr.reveal(`.value__content, .contact__images`,{origin: 'right'})
